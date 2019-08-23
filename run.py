70f17bad7623b93242e2f6d000a92b93c7c19987
AVanurin <48651864+AVanurin@users.noreply.github.com>
2019-08-23 16:49:48 +0300

    fix -

--- run.py
+++ run.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
 
 		r = c.fetchall()
 
-		print("---SCRIPT\n" + s.replace("--Пишем код на SQL ниже", '').replace('--Не забываем про ";"" на конце :)', '') + "\n---RESULT")
+		print("--SCRIPT\n" + s.replace("--Пишем код на SQL ниже", '').replace('--Не забываем про ";"" на конце :)', '') + "\n--RESULT")
 		
 		table_values = ["\t\t".join([str(value) for value in values]) for values in r]
 		table_values.insert(0, "\t".join([description[0] for description in c.description]))
 		d = "\n".join(table_values)
 		print(d)
 
-		print("\n---ANSWER\n\n---")
+		print("\n--ANSWER\n\n--")
 	except Exception as e:
 		raise e
 		print("SQL код написан с ошибками! Проверьте его на ошибки. Не забудьте перед началом работы запустить init.py")
